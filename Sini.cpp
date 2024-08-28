b529a272761973de68c9eda7dc1cb4d76539d35b
Lennynator <111982871+Lennynator@users.noreply.github.com>
2024-08-28 21:56:56 +0300

    Add files via upload

---
+++ Sini.cpp
@@ -0,0 +1,51 @@
+/*
+
+  Harjoitus 1
+
+  Optimoi sini-funktion käsittely. Älä muuta kääntäjäoptioita.
+  Vertaa alkuperäistä ja optimoitua versiota:
+
+  - kuinka paljon enemmän muistia optimoitu versio kuluttaa?
+  - kuinka paljon nopeammin optimoitu versio toimii?
+
+  Käännös: gcc sin.c -lm -o sin
+
+*/
+
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+#define DEG_TO_RAD(A) (A / 57.296)
+#define ITERATIONS 1000000
+
+
+int main()
+{
+	clock_t t1, t2;
+	unsigned i, a;
+	double val;
+
+	double sinit[360];
+
+	for (int kulma = 0; kulma < 360; ++kulma)
+		sinit[kulma] = sin(DEG_TO_RAD(kulma));
+
+
+	t1 = clock();
+	for (i = 0; i < ITERATIONS; ++i)
+	{
+		for (a = 0; a < 360; ++a)
+		{
+			/*
+			  OptimizeIt!
+			*/
+			val = sinit[a];
+			//val = sin(DEG_TO_RAD(a));
+		}
+	}
+	t2 = clock();
+	printf("%.1fs\n", (t2 - t1) / (float)CLOCKS_PER_SEC);
+
+	return 0;
+}
